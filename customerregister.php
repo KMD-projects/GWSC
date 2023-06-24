14e33a4e9912c57552f6ebe450d11d845a6c1318
Emrys <myolwinoo919@gmail.com>
2023-06-25 00:40:13 +0700

    register design

--- customerregister.php
+++ customerregister.php
@@ -6,6 +6,8 @@ use db\model\Customer;
 
 include('db/query.php');
 
+$error = "";
+
 if (isset($_POST['btnRegister'])) {
 
     $customer = new Customer();
@@ -17,13 +19,12 @@ if (isset($_POST['btnRegister'])) {
     $customer->address = mysqli_real_escape_string($connect, $_POST['txtAddress']);
 
     if (isCustomerExists($customer->email)) {
-        echo "<script>window.alert('Customer with email already existed.')</script>";
-        echo "<script>window.location='customerregister.php'</script>";
+        $error = "An account with email (".$customer->email.") already existed.";
     } else {
         if (insertCustomer($customer)) {
-            echo "Customer registered successfully!";
+            header('location: customerlogin.php');
         } else {
-            echo "Failed to register customer.";
+            $error = "Failed to create an account.";
         }
     }
 }
@@ -37,29 +38,44 @@ if (isset($_POST['btnRegister'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Customer register</title>
+    <link rel="stylesheet" href="css/style.css">
+    <script src="https://kit.fontawesome.com/9cfc40fa5c.js" crossorigin="anonymous"></script>
 </head>
-<body>
-<form action="customerregister.php" method="POST">
-    <label for="name">Firstname: </label>
-    <input id="name" type="text" name="txtFirstname" placeholder="Enter firstname" required/><br>
+<body class="login-body">
+<main class="container">
+    <h1 class="center-text margin-0">GWSC</h1>
+    <h3 class="center-text margin-top-8">Create New Account</h3>
+    <form action="customerregister.php" method="POST">
+        <label for="name" class="block gwsc-input-label margin-top-8">Firstname</label>
+        <input class="gwsc-input margin-top-4" id="name" type="text" name="txtFirstname" placeholder="Enter firstname" required/><br>
 
-    <label for="name">Lastname: </label>
-    <input id="name" type="text" name="txtLastname" placeholder="Enter lastname" required/><br>
+        <label for="name" class="block gwsc-input-label margin-top-8">Lastname</label>
+        <input class="gwsc-input margin-top-4" id="name" type="text" name="txtLastname" placeholder="Enter lastname" required/><br>
 
-    <label for="email">Email: </label>
-    <input id="email" type="email" name="txtEmail" placeholder="Enter email" required/><br>
+        <label for="email" class="block gwsc-input-label margin-top-8">Email</label>
+        <input class="gwsc-input margin-top-4" id="email" type="email" name="txtEmail" placeholder="Enter email" required/><br>
 
-    <label for="password">Password: </label>
-    <input id="password" type="password" name="txtPassword" placeholder="Enter password" required/><br>
+        <label for="password" class="block gwsc-input-label margin-top-8">Password</label>
+        <input class="gwsc-input margin-top-4" id="password" type="password" name="txtPassword" placeholder="Enter password" required/><br>
 
-    <label for="phone">Phone: </label>
-    <input id="phone" type="tel" name="txtPhone" placeholder="Enter phone" required/><br>
+        <label for="phone" class="block gwsc-input-label margin-top-8">Phone</label>
+        <input class="gwsc-input margin-top-4" id="phone" type="tel" name="txtPhone" placeholder="Enter phone" required/><br>
 
-    <label for="address">Address: </label>
-    <input id="address" type="text" name="txtAddress" placeholder="Enter address" required/><br>
+        <label for="address" class="block gwsc-input-label margin-top-8">Address</label>
+        <input class="gwsc-input margin-top-4" id="address" type="text" name="txtAddress" placeholder="Enter address" required/><br>
 
-    <input type="submit" name="btnRegister" value="Register">
-    <input type="reset" name="btnReset" value="Cancel">
-</form>
+        <input class="btn-lg-filled margin-top-18" type="submit" name="btnRegister" value="Register">
+    </form>
+    <?php if (!empty($error)) : ?>
+        <div class="gwsc-error center-text margin-top-8">
+            <i class="fa-solid fa-circle-exclamation"></i>
+            <?php
+            echo $error;
+            ?>
+        </div>
+    <?php endif ?>
+    <span class="block center-text margin-top-18">Already have an account?</span>
+    <button class="btn-lg-outlined margin-top-8" onclick="location.href='customerlogin.php'">Login</button>
+</main>
 </body>
 </html>
